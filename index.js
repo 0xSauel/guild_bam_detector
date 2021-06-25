6f2fdf1d8f3280837f9d6468ae5aa9c906ff2b69
Undefined <raineydevil2011@gmail.com>
2021-06-25 14:04:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 const { spawn } = require("child_process");
+const Message = require('../tera-message')
 const Path = require("path");
+
 
 class NotifierLauncher
 {
     constructor(mod)
     {
         this.mod = mod;
+        const notifier = this.mod.require ? this.mod.require.notifier : require('tera-notifier')(this.mod)
+        const MSG = new Message(this.mod)
+
+
         this.exEvent = 0;
         this.mod.log("FFS, first line of code is ok")
-        this.installHooks();
+        this.installHooks(this.mod);
 
         const gbdPath = Path.join(__dirname, "/lib/GuildBAMNotifier.dll");
         this.mod.log("Starting Detector...");
+        this.notificationafk("Starting Detector...")
+        this.commands(this.mod, MSG, notifier)
         this.gbd = spawn('dotnet', [gbdPath], {stdio: ['pipe', 'pipe', 'pipe']});
-        this.gbd.on("exit", () => this.mod.log("GBD Exited dunno why"));
+        this.gbd.on("exit", e => this.mod.log(`GBD Exited\n, ${e.toString()}`));
 
         this.mod.hook("S_NOTIFY_GUILD_QUEST_URGENT", 1, ev =>
         {
@@ -21,14 +29,17 @@ class NotifierLauncher
                 case 0:
                     this.exEvent = 10080;
                     this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+                    this.notificationafk("Guild BAM spawn soon")
                     break;
                 case 1:
                     this.exEvent = 10081;
                     this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+                    this.notificationafk("Guild BAM spawned")
                     break;
                 case 3:
                     this.exEvent = 10083;
                     this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+                    this.notificationafk("Guild BAM dead")
                     break;
                 default:
                     this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
@@ -38,20 +49,58 @@ class NotifierLauncher
 
     }
 
-    globalMod()
+    commands(mod, msg, notifier)
     {
-        return this.mod.globalMod;
+        this.mod = mod
+        this.MSG = msg
+
+        this.mod.command.add(["gbd"], (arg) => {
+            if (!arg) {
+                this.mod.settings.enabled = !this.mod.settings.enabled
+                this.MSG.chat("Guild BAM Detector: " + (this.mod.settings.enabled ? this.MSG.BLU("On") : this.MSG.YEL("Off")))
+            } else {
+                switch (arg) {
+                    case "alert":
+                        this.mod.settings.alerted = !this.mod.settings.alerted
+                        this.MSG.chat("alert " + (this.mod.settings.alerted ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+                        break
+                    case "notice":
+                        this.mod.settings.notice = !this.mod.settings.notice
+                        this.MSG.chat("notice " + (this.mod.settings.notice ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+                        break
+                    case "message":
+                        this.mod.settings.messager = !this.mod.settings.messager
+                        this.MSG.chat("message " + (this.mod.settings.messager ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+                        break
+                    case "status":
+                        this.MSG.chat("Guild BAM Detector: " + (this.mod.settings.enabled ? this.MSG.BLU("On") : this.MSG.YEL("Off")))
+                        this.MSG.chat("alert " + (this.mod.settings.alerted ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+                        this.MSG.chat("message " + (this.mod.settings.messager ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+                        this.MSG.chat("notice " + (this.mod.settings.notice ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+                        break
+                    case "debug":
+                        this.gbd.stdin.write(`1003\\n`, 'utf-8')
+                        break
+                    default:
+                        this.MSG.chat("Detector: " + this.MSG.RED("wrong parameter!"))
+                        break
+                }
+            }
+        })
     }
 
     installHooks()
     {
     }
 
-    debug(msg)
-    {
-        if (!this.mod.settings.debug) return;
-        // this.mod.command.message(`<font color="#fff1b5">${msg}</font>`);
-        this.mod.log(`${msg}`);
+
+    notificationafk(msg, timeout) { // timeout in milsec
+        notifier.notifyafk({
+            title: 'NekOWO-Notification',
+            message: msg,
+            wait: false,
+            sound: 'Notification.IM',
+        }, timeout)
     }
 
     destructor(m)
