3548268d5b51ea93f01aa86de3f73bffe2a309ba
Undefined <raineydevil2011@gmail.com>
2021-06-26 12:25:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,38 +21,10 @@ class GuildBamDetector
         this.mod.log("Starting Detector...");
         this.mod.log(this.notifier.toString())
         // this.notification("Starting Detector...")
-        this.commands()
+        // this.commands()
         this.gbd = spawn('dotnet', [gbdPath], {stdio: ['pipe', 'pipe', 'pipe']});
         this.gbd.on("exit", e => this.mod.log(`GBD Exited\n, ${e.toString()}`));
 
-        this.mod.hook("S_NOTIFY_GUILD_QUEST_URGENT", 1, ev =>
-        {
-            switch (ev.type) {
-                case 0:
-                    this.exEvent = 10080;
-                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
-                    // this.notification("Guild BAM spawn soon")
-                    break;
-                case 1:
-                    this.exEvent = 10081;
-                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
-                    // this.notification("Guild BAM spawned")
-                    break;
-                case 3:
-                    this.exEvent = 10083;
-                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
-                    // this.notification("Guild BAM dead")
-                    break;
-                default:
-                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
-            }
-            this.gbd.stdin.write(`${this.exEvent}\\n`, 'utf-8')
-        });
-
-    }
-
-    commands()
-    {
         this.mod.command.add(["gbd"], (arg) => {
             if (!arg) {
                 this.mod.settings.enabled = !this.mod.settings.enabled
@@ -86,7 +58,70 @@ class GuildBamDetector
                 }
             }
         })
+
+
+        this.mod.hook("S_NOTIFY_GUILD_QUEST_URGENT", 1, ev =>
+        {
+            switch (ev.type) {
+                case 0:
+                    this.exEvent = 10080;
+                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+                    // this.notification("Guild BAM spawn soon")
+                    break;
+                case 1:
+                    this.exEvent = 10081;
+                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+                    // this.notification("Guild BAM spawned")
+                    break;
+                case 3:
+                    this.exEvent = 10083;
+                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+                    // this.notification("Guild BAM dead")
+                    break;
+                default:
+                    this.mod.log(`Event type: ${ev.type}, msg: ${ev.quest}`);
+            }
+            this.gbd.stdin.write(`${this.exEvent}\\n`, 'utf-8')
+        });
+
     }
+
+    // commands()
+    // {
+    //     this.mod.command.add(["gbd"], (arg) => {
+    //         if (!arg) {
+    //             this.mod.settings.enabled = !this.mod.settings.enabled
+    //             this.MSG.chat("Guild BAM Detector: " + (this.mod.settings.enabled ? this.MSG.BLU("On") : this.MSG.YEL("Off")))
+    //         } else {
+    //             switch (arg) {
+    //                 case "alert":
+    //                     this.mod.settings.alerted = !this.mod.settings.alerted
+    //                     this.MSG.chat("alert " + (this.mod.settings.alerted ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+    //                     break
+    //                 case "notice":
+    //                     this.mod.settings.notice = !this.mod.settings.notice
+    //                     this.MSG.chat("notice " + (this.mod.settings.notice ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+    //                     break
+    //                 case "message":
+    //                     this.mod.settings.messager = !this.mod.settings.messager
+    //                     this.MSG.chat("message " + (this.mod.settings.messager ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+    //                     break
+    //                 case "status":
+    //                     this.MSG.chat("Guild BAM Detector: " + (this.mod.settings.enabled ? this.MSG.BLU("On") : this.MSG.YEL("Off")))
+    //                     this.MSG.chat("alert " + (this.mod.settings.alerted ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+    //                     this.MSG.chat("message " + (this.mod.settings.messager ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+    //                     this.MSG.chat("notice " + (this.mod.settings.notice ? this.MSG.BLU("on") : this.MSG.YEL("off")))
+    //                     break
+    //                 case "debug":
+    //                     this.gbd.stdin.write(`1003\\n`, 'utf-8')
+    //                     break
+    //                 default:
+    //                     this.MSG.chat("Detector: " + this.MSG.RED("wrong parameter!"))
+    //                     break
+    //             }
+    //         }
+    //     })
+    // }
 
     installHooks()
     {
